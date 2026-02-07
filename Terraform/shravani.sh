3a12870cca0aba57f5dba1bec409856fc2cc66f6
Shravani Devarakonda <shravanychandhanaa28@gmail.com>
2026-02-08 01:59:55 +0530

    changed the script

--- Terraform/shravani.sh
+++ Terraform/shravani.sh
@@ -3,3 +3,5 @@ sudo apt-get update -y && sudo apt-get install -y docker.io
 sudo systemctl enable docker 
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
+docker pull shravanychandhana/8byte-intern-app
+docker run -p 3000:3000 shravanychandhana/8byte-intern-app
