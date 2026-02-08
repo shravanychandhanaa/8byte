1de8705e1c4b8b5b7366dedf9b780156395836f3
Shravani Devarakonda <shravanychandhanaa28@gmail.com>
2026-02-09 00:30:44 +0530

    changed the app.js file

--- Terraform/shravani.sh
+++ Terraform/shravani.sh
@@ -3,5 +3,3 @@ sudo apt-get update -y && sudo apt-get install -y docker.io
 sudo systemctl enable docker 
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
-docker pull shravanychandhanaa/8byte-intern-app
-docker run -d --name 8byte-intern-app -p 3000:3000 shravanychandhanaa/8byte-intern-app
