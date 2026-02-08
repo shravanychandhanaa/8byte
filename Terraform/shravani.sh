b7e0057388bad3cd0eb2a6685c18412c8e9cd532
Shravani Devarakonda <shravanychandhanaa28@gmail.com>
2026-02-08 15:52:39 +0530

    testing ec2 ssh automation

--- Terraform/shravani.sh
+++ Terraform/shravani.sh
@@ -4,4 +4,4 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 docker pull shravanychandhanaa/8byte-intern-app
-docker run -d -p 3000:3000 shravanychandhanaa/8byte-intern-app
+docker run -d --name 8byte-intern-app -p 3000:3000 shravanychandhanaa/8byte-intern-app
